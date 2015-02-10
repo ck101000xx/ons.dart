6b1763050cedc3c8430ee3677b68ae706ad1b3f9
ck101000xx <ck101000xx@gmail.com>
2015-02-10 22:25:29 +0800

    Rewrite the parser

--- lib/script/grammar.dart
+++ lib/script/grammar.dart
@@ -1,34 +1,80 @@
-import 'types.dart'
-class NscripterGrammar extends CompositeParser {
-  @override
-  void initialize() {
-    def('start', ref('statement').star().end());
-    _newline();
-    _whitespace();
-    _expression();
-    _statement();
+import 'package:ons/script/types.dart';
+import 'package:petitparser/petitparser.dart';
+
+
+class NScripterGrammarDefinition extends GrammarDefinition { 
+  start() => ref(statement).star().end();
+  newline() => Token.newlineParser();
+  comment() => char(';') & ref(newline).neg().star();
+  white() =>
+    ref(newline).not() & whitespace() |
+    ref(comment) |
+    ref(newline) & char('/');
+  digits() => digit().plus().flatten();
+  integer() => string('0x').optional().seq(ref(digits)).flatten().map(int.parse);
+  name() =>
+    (letter() | char('_'))
+      .seq((digit() | letter() | char('_')).star())
+      .flatten();
+
+  variable(Type type) {
+    Map<Type, String> prefixes = {
+      double: '%',
+      String: '\$',
+      List:   '?'
+    };
+    return (char(prefixes[type]) & ref(integer).or(ref(name))).pick(1);
   }
-  void _newline() {
-    def('newline', Token.newlineParser());
+
+  expression() {
+    Parser number =
+      (char('+') | char('-')).optional()
+        .seq(ref(digits))
+        .seq(char('.').seq(ref(digits)).optional())
+        .flatten()
+        .map(double.parse);
+
+    Parser string = (char('"') & char('"').neg().star().flatten() & char('"')).pick(1);
+    
+    ExpressionBuilder builder = new ExpressionBuilder();
+    
+    builder.group()
+      ..primitive(number.or(ref(integer)).map((n) => new Constant(n)).trim())
+      ..primitive(string.map((n) => new Constant(n)).trim());
+    
+    builder.group()
+      ..left(char('*').trim(), (a, op, b) => new Mut(a, b))
+      ..left(char('/').trim(), (a, op, b) => new Div(a, b));
+    
+    builder.group()
+      ..left(char('+').trim(), (a, op, b) => new Add(a, b))
+      ..left(char('-').trim(), (a, op, b) => new Sub(a, b));
+
+    return builder.build();
   }
-  void _whitespace() {
-    def('whitespace',
-        (ref('newline').not() & whitespace()) |
-        ref('comment') |
-        (ref('newline') & char('/')));
-    def('comment', char(';') & ref('newline').neg().star());
+
+  condition() {
+    Parser unary(Parser operator, fn) =>
+      (ref(expression) &  operator.trim() & ref(expression)).map((list) => fn(list[0], list[1]));
+
+    Parser cond =
+      (string('fchk') & whitespace().plus() & ref(expression)).map((a) => new FCHK(a)) |
+      unary(char('=').seq(char('=').optional()).trim(), (a, b) => new EQ(a, b)) |
+      unary(string('!=').or(string('<>')).trim(), (a, b) => new NE(a, b)) |
+      unary(string('>=').trim(), (a, b) => new GTE(a, b)) |
+      unary(string('<=').trim(), (a, b) => new LTE(a, b)) |
+      unary(char('>').trim(), (a, b) => new GT(a, b)) |
+      unary(char('<').trim(), (a, b) => new LT(a, b));
+
+    ExpressionBuilder builder = new ExpressionBuilder();
+
+    builder.group().primitive(cond.trim());
+    
+    builder.group()
+       .left(char('&').seq(char('&').optional()).trim(), (a, op, b) => new And(a, b));
+        
+    return builder.build();  
   }
-  void _expression() {
-    Parser digits = digit().plus().flatten();
-    Parser signed = char('+').or(char('-')).optional().seq(p).flatten();
-    Parser integer = (digits).map(int.parse);
-    Parser number = (signed & (char('.') & digits).flatten().optional()).flatten().map((s) => new Number(double.parse(s)));
-    Parser numberVariable = (char('%') & integer).pick(1).map((i) => new NumberVariable(i));
-    Parser stringVariable = (char('$') & letter().or(digit()).or('_').plus().flatten()).pick(1).map((s) => new StringVariable(s));
-    Parser arrayVariable = ((char('?') & integer).pick(1) & (char('[') & integer & char(']')).pick(1).star()).map((l) => new ArrayVariable(l[0], l[1]))
-  }
-  void _statement() {
-    def('statement seperator', char(':') | (ref('newline') & char('/').not()));
-    def('label', char('*').seq(ref('newline').neg().star()).seq('newline');
-  }
+
+  label() => char('*').seq(ref(name)).pick(1).map((name) => new Label(name));
 }
--- lib/script/types.dart
+++ lib/script/types.dart
@@ -1,25 +1,78 @@
-typedef Script = List<Statement>
 class Expression {
 }
 class Variable implements Expression {
+  final Type type;
+  final index;
+  Variable(this.type, this.index);
 }
-class Constant<T> implements Expression {
-  T value;
-  Constant(v) : value(v) {
-  }
+class Constant implements Expression {
+  final value;
+  Constant(this.value);
+}
+class Add implements Expression {
+  final Expression left, right;
+  Add(this.left, this.right);
+}
+class Sub implements Expression {
+  final Expression left, right;
+  Sub(this.left, this.right);
+}
+class Mut implements Expression {
+  final Expression left, right;
+  Mut(this.left, this.right);
+}
+class Div implements Expression {
+  final Expression left, right;
+  Div(this.left, this.right);
+}
+class Condition {
+}
+class GT implements Condition {
+  final Expression left, right;
+  GT(this.left, this.right);  
+}
+class GTE implements Condition {
+  final Expression left, right;
+  GTE(this.left, this.right);  
+}
+class LT implements Condition {
+  final Expression left, right;
+  LT(this.left, this.right);  
+}
+class LTE implements Condition {
+  final Expression left, right;
+  LTE(this.left, this.right);  
+}
+class EQ implements Condition {
+  final Expression left, right;
+  EQ(this.left, this.right);  
+}
+class NE implements Condition {
+  final Expression left, right;
+  NE(this.left, this.right);  
+}
+class FCHK implements Condition {
+  final Expression str;
+  FCHK(this.str);  
+}
+class And implements Condition {
+  final Condition left, right;
+  And(this.left, this.right);  
 }
 class Statement {
+}
+class Block implements Statement {
+  final List<Statement> statements;
+  Block(this.statements);
 }
 class ControlStatement implements Statement {
 }
 class Label implements ControlStatement {
   final String name;
-  Label(String str) : name(str) {
-  }
+  Label(this.name);
 }
 class If implements ControlStatement {
   final Expression expression;
   final Statement statement;
-  If(Expression e, Statement s) : expression(e) : statement(s) {
-  }
+  If(this.expression, this.statement);
 }
